d8a8818dcc7eb291c3a1b337e0e1066cad411188
dodamce <3030949391@qq.com>
2023-03-07 22:21:32 +0800

    删除服务器日志代码

--- src/main/java/servlet/log.java
+++ src/main/java/servlet/log.java
@@ -26,7 +26,7 @@ public class log extends HttpServlet {
         // 获取请求参数，和数据库的内容进行比较，比较成功创建会话
         String name = req.getParameter("name");
         String password = req.getParameter("password");
-        System.out.println("DEBUG:" + name + " " + password);
+//        System.out.println("DEBUG:" + name + " " + password);
         if (null == name || "".equals(name) || null == password || "".equals(password)) {
             // 数据不全
             resp.setContentType("text/html; charset=utf8");
